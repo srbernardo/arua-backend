c8d84735741532d3694fefc1a22d44a1a322bfd5
Bernardo Brotelli <b.brotelle@gmail.com>
2026-08-14 19:41:01 +0100

    Update schema to add admins table and increment version

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_08_02_194214) do
+ActiveRecord::Schema[8.1].define(version: 2026_08_14_174856) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -53,6 +53,14 @@ ActiveRecord::Schema[8.1].define(version: 2026_08_02_194214) do
     t.bigint "user_id", null: false
     t.string "zip", null: false
     t.index ["user_id"], name: "index_addresses_on_user_id"
+  end
+
+  create_table "admins", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
   end
 
   create_table "cart_items", force: :cascade do |t|
