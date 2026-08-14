f41a533a705bfb6ac9941b11ad6f79fe3a61503f
Bernardo Brotelli <b.brotelle@gmail.com>
2026-08-14 20:32:31 +0100

    Configure Active Storage for test environment with local service and route drawing

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -47,4 +47,8 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions.
   config.action_controller.raise_on_missing_callback_actions = true
+
+  # Active Storage
+  config.active_storage.service = :local
+  config.active_storage.draw_routes = true
 end
